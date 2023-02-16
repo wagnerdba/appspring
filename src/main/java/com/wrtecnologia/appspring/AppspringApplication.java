95517bcd1b259cbf852215d229abe06807fea611
wagnerdba <125572996+wagnerdba@users.noreply.github.com>
2023-02-16 17:17:20 -0300

    @Slf4j
    @Slf4j

--- src/main/java/com/wrtecnologia/appspring/AppspringApplication.java
+++ src/main/java/com/wrtecnologia/appspring/AppspringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class AppspringApplication {
 
 	public static void main(String[] args) {

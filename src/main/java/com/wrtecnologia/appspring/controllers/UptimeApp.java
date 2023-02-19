c0d5525ccc68807dd658d31d604ab91c78a1b3fc
wagnerdba <125572996+wagnerdba@users.noreply.github.com>
2023-02-19 09:32:26 -0300

    Update com n de dias aumentado

--- src/main/java/com/wrtecnologia/appspring/controllers/UptimeApp.java
+++ src/main/java/com/wrtecnologia/appspring/controllers/UptimeApp.java
@@ -29,7 +29,10 @@ public class UptimeApp {
 
         long millis = System.currentTimeMillis() - start;
 
-        String uptime = String.format("%02d:%02d:%02d:%02d", TimeUnit.MILLISECONDS.toDays(millis), TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)), TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+        String uptime = String.format("%03d:%02d:%02d:%02d", TimeUnit.MILLISECONDS.toDays(millis),
+                                                             TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)),
+                                                             TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                                                             TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
 
         // LocalDateTime date = LocalDateTime.now().plusDays(4); // ADD DAYS IN DATE
         LocalDateTime date = LocalDateTime.now();
@@ -37,9 +40,11 @@ public class UptimeApp {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE dd/MM/uuuu HH:mm:ss");
 
         return String.format(
-                "<p><strong><font color=\"blue\" face=\"Verdana\" size=\"2\">Data e hora servidor...: </strong>" + date.format(formatter) + "</font>" +
-                "<strong><font color=\"black\" face=\"Verdana\" size=\"2\"><br>Start aplicação............: </strong>" + appHealth.startapp + "</font>" +
-                "<strong><font color=\"black\" face=\"Verdana\" size=\"2\"><br>Uptime aplicação.........:</strong> %s", uptime) + "</font><br>" +
-                "<strong><font color=\"black\" face=\"Verdana\" size=\"2\">Próxima execução job.: </strong>" + diversosJobsDateTime.next() + "</font></p>";
+                "<p><strong><font color=\"red\" face=\"Courier\" size=\"3\">Railway App Server (AppspringApplication)</strong></p>" +
+                "<p><strong><font color=\"blue\" face=\"Courier\" size=\"2\">Server date/time..: </strong>" + date.format(formatter) + "</font>" +
+                "<strong><font color=\"black\" face=\"Courier\" size=\"2\"><br>Start in..........: </strong>" + appHealth.startapp + "</font>" +
+                "<strong><font color=\"black\" face=\"Courier\" size=\"2\"><br>Uptime............:</strong> %s", uptime) + "</font><br>" +
+                "<strong><font color=\"black\" face=\"Courier\" size=\"2\">Next execution job: </strong>" + diversosJobsDateTime.next() + "</font></p>" +
+                "<p><strong><font color=\"black\" face=\"Courier\" size=\"2\">by Wagner Pires (dev)</font></strong></p>";
     }
 }
